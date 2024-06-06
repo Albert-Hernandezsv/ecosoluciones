3902e0996e5ef200d633c7496047b6d1bae5d392
Nelson Moran <mm19037@ues.edu.sv>
2024-06-06 15:43:47 -0600

    Agregando parte 4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Correos
+function SendMail() {
+  var params = {
+    from_name : document.getElementById("nombre").value,
+    email_id : document.getElementById("correo").value,
+    message : document.getElementById("mensaje").value
+  }
+  emailjs.send("service_xy6rxfn", "template_uz5rusq", params).then(function (res) {
+    alert("succes! " + res.status)
+  })
+}
+
 // Menú desplegable
 const buttonMenu = document.querySelector('#nav-mobile');
 const navMenu = document.querySelector('.nav-menu');
